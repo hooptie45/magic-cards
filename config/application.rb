0a2b175373dfc3241648d0b12b18ee73d6e7359b
Shaun Hannah <shannah3@bloomberg.net>
2017-10-03 13:11:00 -0400

    Default ACTION_CABLE_ALLOWED_REQUEST_ORIGINS

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module MagicCards
     config.action_cable.url = ENV['ACTION_CABLE_FRONTEND_URL']
 
     # Action Cable setting to allow connections from these domains.
-    origins = ENV['ACTION_CABLE_ALLOWED_REQUEST_ORIGINS'].split(',')+ ["172.18.0.1"]
+    origins = ENV.fetch('ACTION_CABLE_ALLOWED_REQUEST_ORIGINS', "localhost,172.18.0.1").split(',')
 
 
     origins.map! { |url| /#{url}/ }
